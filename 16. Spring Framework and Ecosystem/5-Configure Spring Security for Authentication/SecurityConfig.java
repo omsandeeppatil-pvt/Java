7fe24a25ef27b8aefcc922c74a2c85adf9fd78d3
Om Patil <159125383+omsandeeppatil-pvt@users.noreply.github.com>
2025-01-03 13:46:55 +0530

    Create SecurityConfig.java

---
+++ 16. Spring Framework and Ecosystem/5-Configure Spring Security for Authentication/SecurityConfig.java
@@ -0,0 +1,18 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests()
+                .anyRequest().authenticated()
+            .and()
+            .formLogin();
+        return http.build();
+    }
+}
